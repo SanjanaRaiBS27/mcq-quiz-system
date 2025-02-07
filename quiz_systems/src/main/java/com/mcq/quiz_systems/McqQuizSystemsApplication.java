4fb20a1798329e897ca7ca2dead99ad62090d8a9
Sanjana <sanjana@APPLEs-MacBook-Pro.local>
2025-02-07 15:06:25 +0530

    Added project files

---
+++ quiz_systems/src/main/java/com/mcq/quiz_systems/McqQuizSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.mcq.quiz_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqQuizSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McqQuizSystemsApplication.class, args);
+	}
+
+}
---
+++ quiz_systems/src/test/java/com/mcq/quiz_systems/McqQuizSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcq.quiz_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McqQuizSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
